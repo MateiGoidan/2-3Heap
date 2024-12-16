7e8e48045d84723d57d8c80c659ff07192aaf267
MateiGoidan <goidan.mateiconstantin@gmail.com>
2024-12-16 17:05:10 +0200

    Start of the Data Structure.

---
+++ main.cpp
@@ -0,0 +1,59 @@
+/*
+ * 2-3 Heap Implementation
+ *
+ * Goidan Matei-Constantin
+ *
+ */
+
+#include <iostream>
+
+// Node structure
+struct Node {
+  Node* parent;
+  Node* children[];
+};
+
+void init(int N);
+
+void add(int i, int x);
+
+int get_min(int i);
+
+void decrease_key(int i, int x);
+
+void merge_sets(int i, int j);
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(0);
+
+  int N, _, op, i, j, x;
+
+  std::cin >> N >> _;
+
+  init(N);
+
+  do {
+    std::cin >> op >> i;
+    switch (op) {
+    case 1:
+      std::cin >> x;
+      add(i, x);
+      break;
+    case 2:
+      std::cout << get_min(i) << '\n';
+
+      break;
+    case 3:
+      std::cin >> x;
+      decrease_key(i, x);
+      break;
+    case 4:
+      std::cin >> j;
+      merge_sets(i, j);
+      break;
+    }
+  } while (_ -= 1); // Altfel se vede ciudat si se copiaza gresit.
+
+  return 0;
+}
