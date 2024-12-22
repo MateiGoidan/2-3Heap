25001f925df29ef6c02a19dcf9182d93ffac7dd7
MateiGoidan <goidan.mateiconstantin@gmail.com>
2024-12-22 16:15:18 +0200

    Created the structure.

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+void init(int N);
+
+void add(int i, int x);
+
+int get_min(int i);
+
+void decrease_key(int i, int x);
+
+void merge_sets(int i, int j);
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+
+	int N, _, op, i, j, x;
+
+	std::cin>>N>>_;
+
+	init(N);
+
+	do
+	{
+		std::cin>>op>>i;
+		switch(op)
+		{
+		case 1:
+			std::cin>>x;
+			add(i, x);
+			break;
+		case 2:
+			std::cout<<get_min(i)<<'n'; // Va rog modificati aici, polygon nu permite afisarea de backslash (sau cel putin nu stiu eu cum)
+			break;
+		case 3:
+			std::cin>>x;
+			decrease_key(i, x);
+			break;
+		case 4:
+			std::cin>>j;
+			merge_sets(i, j);
+			break;
+		}
+	}while(_-=1); // Altfel se vede ciudat si se copiaza gresit.
+
+	return 0;
+}
